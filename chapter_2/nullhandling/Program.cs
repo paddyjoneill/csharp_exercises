1fb428c7571157a84c2b91e444239bdfc56154f8
Patrick O'Neill <paddyjoneill@hotmail.com>
2020-08-21 11:19:46 +0100

    working through chapter 2

---
+++ chapter_2/nullhandling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace nullhandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int thisCannotBeNull = 4;
+            // thisCannotBeNull = null; // gives compile error
+            
+
+            int? thisCouldBeNull = null;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            thisCouldBeNull = 7;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+        }
+    }
+}
